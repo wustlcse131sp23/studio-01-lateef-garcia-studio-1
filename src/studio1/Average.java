ecb5441530373dad663c3b85a511e18d922477a3
f.a.lateef <f.a.lateef@1V000WL-602WGL3.accounts.ad.wustl.edu>
2023-01-26 15:16:32 -0500

    Studio 1

---
+++ src/studio1/Average.java
@@ -0,0 +1,17 @@
+package studio1;
+import java.util.Scanner;
+
+public class Average {
+
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		System.out.print("Enter value: ");
+		double n1 = in.nextInt();
+		System.out.print("Enter value: ");
+		int n2 = in.nextInt();
+		System.out.println((n1 + n2) / 2 );
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/studio1/LeapYear.java
@@ -0,0 +1,18 @@
+package studio1;
+import java.util.Scanner;
+
+public class LeapYear {
+
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		System.out.print("Enter value: ");
+		int n1 = in.nextInt();
+		System.out.print(n1 "is +  a + leap + year");
+		System.out.print("Enter value: ");
+		int n2 = in.nextInt();
+		System.out.println( );
+		// TODO Auto-generated method stub
+
+	}
+
+}
